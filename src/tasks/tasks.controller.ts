02d4188deb6be0f6917f990374cdbd99df8d44b8
Pavel <artpavel@ukr.net>
2022-08-04 11:43:41 +0300

    add getbyid

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,22 +1,27 @@
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
+  // get all
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
 
+  // get by id
+  @Get(':id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   // create task
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    return this.tasksService.cteateTask(title, description);
+  createTask(@Body() dto: CreateTaskDto): Task {
+    return this.tasksService.cteateTask(dto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,3 +1,4 @@
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task, TaskStatus } from './task.model';
 import { Injectable } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
@@ -6,17 +7,22 @@ import { v4 as uuid } from 'uuid';
 export class TasksService {
   private tasks: Task[] = [];
 
+  // Get All
   getAllTasks(): Task[] {
     return this.tasks;
   }
 
+  // Get by id
+  getTaskById(id: string): Task {
+    return this.tasks.find((t) => t.id === id);
+  }
+
   // create task
-  cteateTask(title: string, description: string): Task {
+  cteateTask(dto: CreateTaskDto): Task {
     const task: Task = {
       id: uuid(),
-      title,
-      description,
       status: TaskStatus.OPEN,
+      ...dto,
     };
 
     this.tasks.push(task);
